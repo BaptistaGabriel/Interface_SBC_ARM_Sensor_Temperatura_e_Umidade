8e5d0d568326b6e9014d0f43cc0fff7689bc5900
MarcioDzn <marcio.roberto0739@gmail.com>
2023-12-02 18:37:40 -0300

    feat: add memory mapping function

--- gpio.s
+++ gpio.s
@@ -1,4 +1,21 @@
 mapMemory
+    ldr r0, =devMem
+    mov r1, #2
+    mov r7, #5
+    svc 0
+    mov r4, r0 @ pega o arquivo que dá acesso à memória física
+
+    ldr r5, =gpioaddr @ pega o endereço base 0x1C20
+    ldr r5, [r5]
+    ldr r1, =pagelen @ pega o tamanho da página - 1k (4096)
+    ldr r1, [r1]
+    mov r2, #3 
+    mov r3, #1
+    mov r0, #0 @ so escolhe o endereço da memória virtual
+    mov r7, #192 @sysmmap2
+    svc 0
+    mov r8, r0
+    add r8, #0x800 @ offset pra pegar o endereço base da GPIO 0x1C20800
 
     bx lr
 
