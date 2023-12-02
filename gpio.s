f8754528a41a3a5fdaf448381dafd1ac1812dc4c
MarcioDzn <marcio.roberto0739@gmail.com>
2023-12-02 18:34:43 -0300

    feat: add setCondPinState
    function that defines a 0 or 1 state for a pin depending on a certain value

--- gpio.s
+++ gpio.s
@@ -58,7 +58,7 @@ setPinHigh:
     @ |1|000 -> 000|1| e depois substitui isso no r2 (registrador de dados)
     mov r0, #1
     lsl r0, r3
-    orr r2, r0
+    orr r2, r2, r0
 
     @ guardando novamente na memória
     str r2, [r8, r1]
@@ -75,7 +75,7 @@ setPinLow:
     ldr r2, [r8, r1] @ carrega o registrador de dados do pino
     
     @ criando a máscara e substituindo o registrador de dados
-    @ |1|000 -> 000|1| e depois substitui isso no r2 (registrador de dados)
+    @ |0|000 -> 000|0| e depois substitui isso no r2 (registrador de dados)
     mov r0, #1
     lsl r0, r3
     bic r2, r0
@@ -100,3 +100,38 @@ getPinState:
     and r1, r2, r0
 
     bx lr
+
+
+@ /////////////////////////////////////////////////////////////////////////////////////////////
+@ //          Verifica se um bit é 0 ou 1 e adiciona esse estado a d7, d6, d5 ou d4          //
+@ /////////////////////////////////////////////////////////////////////////////////////////////
+setCondPinState:
+    @ pegando o estado do pino
+    mov r4, #1
+    mov r3, r9
+    lsr r3, r2 @ move o bit (na posição em r2) pra o LSB
+    and r1, r4, r3 @ atribui 0 ou 1 para r1 (a depender do estado do bit)
+
+    @ pegando o registrador de dados
+    ldr r3, [r5, #8]
+    ldr r2, [r5, #12]
+    ldr r6, [r8, r2] @ coloca o registrador de dados em r6
+
+    @ cria máscara ...0000|1| -> ...00|1|00 (r2 vezes)
+    mov r4, #1
+    lsl r4, r2
+
+    @ verificando se o estado do bit é 0 ou 1
+    cmp r1, #0
+    beq lowState 
+
+    @ highState
+    orr r6, r4
+    str r6, [r8, r3]
+    bx lr
+
+    lowState:
+        bic r6, r4
+        str r6, [r8, r3]
+        bx lr
+    
