74e20bc786f5c35139ab6a327da41483504fe6a7
MarcioDzn <marcio.roberto0739@gmail.com>
2023-12-02 18:30:25 -0300

    feat: add lcd write

--- display.s
+++ display.s
@@ -238,8 +238,83 @@ clearDisplay:
     bl setPinHigh
 
     @ enable
-    
+
     ldr lr, [sp]
     add sp, sp, #4
 
     bx lr
+
+writeChar:
+    sub sp, sp, #4
+    str lr, [sp]
+
+    ldr r9, [r9]
+
+    ldr r0, =RS
+    bl setPinHigh
+
+    mov r2, #7
+    mov r5, =D7
+    bl setCondPinState
+
+    mov r2, #6
+    mov r5, =D6
+    bl setCondPinState
+
+    mov r2, #5
+    mov r5, =D5
+    bl setCondPinState
+
+    mov r2, #4
+    mov r5, =D4
+    bl setCondPinState
+
+    @ enable
+
+    mov r2, #3
+    mov r5, =D7
+    bl setCondPinState
+
+    mov r2, #2
+    mov r5, =D6
+    bl setCondPinState
+
+    mov r2, #1
+    mov r5, =D5
+    bl setCondPinState
+
+    mov r2, #0
+    mov r5, =D4
+    bl setCondPinState
+
+    @enable
+
+    ldr lr, [sp]
+    add sp, sp, #4
+
+    bx lr
+
+writeWord:
+    @ r0 - palavra
+    @ r11 - tamanho da palavra 
+    @ exemplo: ldr r0, =palavra 
+
+    sub sp, sp, #4
+    str lr, [sp]
+
+    mov r12, #0
+    loopWrite:
+        mov r9, [r0, r12]
+        bl writeChar
+
+        cmp r12, r11
+        beq endWrite
+
+        add r12, r12, #1
+        b loopWrite
+
+    endWrite:
+        ldr lr, [sp]
+        add sp, sp, #4
+        
+        bx lr
